6b84ac84a1306f44dad7870d6df10881a33b55b9
Chintu jha <67228053+chintu72100@users.noreply.github.com>
2023-12-23 20:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Get the body element
+        var body = document.body;
+
+        // Check if the body is empty
+        if (body.innerHTML.trim() === '') {
+            // If the body is empty, write the string "DOM load success"
+            body.innerHTML = 'DOM load success';
+        }
+    });
+</script>
